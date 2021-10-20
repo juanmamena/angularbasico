e12a52df4f3fcbf81138f83df4d489cd1337761b
juanmamena <juanma.mena@icloud.com>
2021-10-20 01:06:10 -0500

    Cambios finales seccion 5

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+})
+export class PersonajesComponent {
+  get personajes(): Personaje[] {
+    return this.dbzService.personajes;
+  }
+  constructor(private dbzService: DbzService) {}
+}
